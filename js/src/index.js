75e16e46a66c83c9622e078d20533d30b9542540
Joe Parsons <parsonsj@email.arizona.edu>
2019-07-19 11:45:46 -0700

    Cleaned up twbs JS module imports.

--- js/src/index.js
+++ js/src/index.js
@@ -1,15 +1,17 @@
-import { Alert } from '../../node_modules/bootstrap/js/src/index.js'
-import { Button } from '../../node_modules/bootstrap/js/src/index.js'
-import { Carousel } from '../../node_modules/bootstrap/js/src/index.js'
-import { Collapse } from '../../node_modules/bootstrap/js/src/index.js'
-import { Dropdown } from '../../node_modules/bootstrap/js/src/index.js'
-import { Modal } from '../../node_modules/bootstrap/js/src/index.js'
-import { Popover } from '../../node_modules/bootstrap/js/src/index.js'
-import { Scrollspy } from '../../node_modules/bootstrap/js/src/index.js'
-import { Tab } from '../../node_modules/bootstrap/js/src/index.js'
-import { Toast } from '../../node_modules/bootstrap/js/src/index.js'
-import { Tooltip } from '../../node_modules/bootstrap/js/src/index.js'
-import { Util } from '../../node_modules/bootstrap/js/src/index.js'
+import {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Popover,
+  Scrollspy,
+  Tab,
+  Toast,
+  Tooltip,
+  Util
+} from '../../node_modules/bootstrap/js/src/index.js'
 
 /**
  * --------------------------------------------------------------------------
