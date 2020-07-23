6418c64fec52663f818b9ab8a1ab265b8a239181
Chris Green <chrisgreen@email.arizona.edu>
2020-07-23 13:18:25 -0700

    update js workflow

---
+++ build/banner.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const pkg = require('../package.json')
+const year = new Date().getFullYear()
+
+function getBanner(pluginFilename) {
+  return `/*!
+  * Arizona Bootstrap${pluginFilename ? ` ${pluginFilename}` : ''} v${pkg.version} (${pkg.homepage})
+  * Copyright ${year} The Arizona Board of Regents on behalf of The University of Arizona
+  * Licensed under ${pkg.license} (https://github.com/az-digital/arizona-bootstrap/blob/master/LICENSE)
+  */`
+}
+
+module.exports = getBanner
