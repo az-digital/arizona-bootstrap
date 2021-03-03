b26c399a2dce152d5bd91903f1e04e5c562aa1ca
Joe Parsons <parsonsj@email.arizona.edu>
2021-03-03 14:46:22 -0700

    Fixed Bootstrap imports in index.js.

--- js/src/index.js
+++ js/src/index.js
@@ -1,22 +1,20 @@
-import {
-  Alert,
-  Button,
-  Carousel,
-  Collapse,
-  Dropdown,
-  Modal,
-  Popover,
-  Scrollspy,
-  Tab,
-  Toast,
-  Tooltip,
-  Util
-} from '../../node_modules/bootstrap/js/src/index.js'
+import Alert from '../../node_modules/bootstrap/js/src/alert.js'
+import Button from '../../node_modules/bootstrap/js/src/button.js'
+import Carousel from '../../node_modules/bootstrap/js/src/carousel.js'
+import Collapse from '../../node_modules/bootstrap/js/src/collapse.js'
+import Dropdown from '../../node_modules/bootstrap/js/src/dropdown.js'
+import Modal from '../../node_modules/bootstrap/js/src/modal.js'
+import Popover from '../../node_modules/bootstrap/js/src/popover.js'
+import Scrollspy from '../../node_modules/bootstrap/js/src/scrollspy.js'
+import Tab from '../../node_modules/bootstrap/js/src/tab.js'
+import Toast from '../../node_modules/bootstrap/js/src/toast.js'
+import Tooltip from '../../node_modules/bootstrap/js/src/tooltip.js'
+import Util from '../../node_modules/bootstrap/js/src/util.js'
 import Offcanvasmenu from './offcanvasmenu'
 
 /**
  * --------------------------------------------------------------------------
- * Bootstrap (v4.5.2): index.js
+ * Bootstrap (v4.6.0): index.js
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  * --------------------------------------------------------------------------
  */
