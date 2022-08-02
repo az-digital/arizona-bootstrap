85bc5fffe805089177d4f83220cc9cbf0321a2b9
Martin Munro <mmunro@ltrr.arizona.edu>
2022-08-01 17:03:16 -0700

    Bump some dev dependency versions.

--- dist/js/arizona-bootstrap.bundle.js
+++ dist/js/arizona-bootstrap.bundle.js
@@ -15,7 +15,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): util.js
+   * Bootstrap (v4.6.2): util.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -207,7 +207,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): alert.js
+   * Bootstrap (v4.6.2): alert.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -217,7 +217,7 @@
    */
 
   const NAME$b = 'alert';
-  const VERSION$a = '4.6.1';
+  const VERSION$a = '4.6.2';
   const DATA_KEY$b = 'bs.alert';
   const EVENT_KEY$b = `.${DATA_KEY$b}`;
   const DATA_API_KEY$8 = '.data-api';
@@ -364,7 +364,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): button.js
+   * Bootstrap (v4.6.2): button.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -374,7 +374,7 @@
    */
 
   const NAME$a = 'button';
-  const VERSION$9 = '4.6.1';
+  const VERSION$9 = '4.6.2';
   const DATA_KEY$a = 'bs.button';
   const EVENT_KEY$a = `.${DATA_KEY$a}`;
   const DATA_API_KEY$7 = '.data-api';
@@ -559,7 +559,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): carousel.js
+   * Bootstrap (v4.6.2): carousel.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -569,7 +569,7 @@
    */
 
   const NAME$9 = 'carousel';
-  const VERSION$8 = '4.6.1';
+  const VERSION$8 = '4.6.2';
   const DATA_KEY$9 = 'bs.carousel';
   const EVENT_KEY$9 = `.${DATA_KEY$9}`;
   const DATA_API_KEY$6 = '.data-api';
@@ -1154,7 +1154,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): collapse.js
+   * Bootstrap (v4.6.2): collapse.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -1164,7 +1164,7 @@
    */
 
   const NAME$8 = 'collapse';
-  const VERSION$7 = '4.6.1';
+  const VERSION$7 = '4.6.2';
   const DATA_KEY$8 = 'bs.collapse';
   const EVENT_KEY$8 = `.${DATA_KEY$8}`;
   const DATA_API_KEY$5 = '.data-api';
@@ -4146,7 +4146,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): dropdown.js
+   * Bootstrap (v4.6.2): dropdown.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -4156,7 +4156,7 @@
    */
 
   const NAME$7 = 'dropdown';
-  const VERSION$6 = '4.6.1';
+  const VERSION$6 = '4.6.2';
   const DATA_KEY$7 = 'bs.dropdown';
   const EVENT_KEY$7 = `.${DATA_KEY$7}`;
   const DATA_API_KEY$4 = '.data-api';
@@ -4664,7 +4664,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): modal.js
+   * Bootstrap (v4.6.2): modal.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -4674,7 +4674,7 @@
    */
 
   const NAME$6 = 'modal';
-  const VERSION$5 = '4.6.1';
+  const VERSION$5 = '4.6.2';
   const DATA_KEY$6 = 'bs.modal';
   const EVENT_KEY$6 = `.${DATA_KEY$6}`;
   const DATA_API_KEY$3 = '.data-api';
@@ -5277,7 +5277,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): tools/sanitizer.js
+   * Bootstrap (v4.6.2): tools/sanitizer.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -5406,7 +5406,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): tooltip.js
+   * Bootstrap (v4.6.2): tooltip.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -5416,7 +5416,7 @@
    */
 
   const NAME$5 = 'tooltip';
-  const VERSION$4 = '4.6.1';
+  const VERSION$4 = '4.6.2';
   const DATA_KEY$5 = 'bs.tooltip';
   const EVENT_KEY$5 = `.${DATA_KEY$5}`;
   const JQUERY_NO_CONFLICT$5 = $__default["default"].fn[NAME$5];
@@ -6164,7 +6164,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): popover.js
+   * Bootstrap (v4.6.2): popover.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -6174,7 +6174,7 @@
    */
 
   const NAME$4 = 'popover';
-  const VERSION$3 = '4.6.1';
+  const VERSION$3 = '4.6.2';
   const DATA_KEY$4 = 'bs.popover';
   const EVENT_KEY$4 = `.${DATA_KEY$4}`;
   const JQUERY_NO_CONFLICT$4 = $__default["default"].fn[NAME$4];
@@ -6332,7 +6332,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): scrollspy.js
+   * Bootstrap (v4.6.2): scrollspy.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -6342,7 +6342,7 @@
    */
 
   const NAME$3 = 'scrollspy';
-  const VERSION$2 = '4.6.1';
+  const VERSION$2 = '4.6.2';
   const DATA_KEY$3 = 'bs.scrollspy';
   const EVENT_KEY$3 = `.${DATA_KEY$3}`;
   const DATA_API_KEY$2 = '.data-api';
@@ -6451,7 +6451,7 @@
 
           return null
         })
-        .filter(item => item)
+        .filter(Boolean)
         .sort((a, b) => a[0] - b[0])
         .forEach(item => {
           this._offsets.push(item[0]);
@@ -6640,7 +6640,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): tab.js
+   * Bootstrap (v4.6.2): tab.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -6650,7 +6650,7 @@
    */
 
   const NAME$2 = 'tab';
-  const VERSION$1 = '4.6.1';
+  const VERSION$1 = '4.6.2';
   const DATA_KEY$2 = 'bs.tab';
   const EVENT_KEY$2 = `.${DATA_KEY$2}`;
   const DATA_API_KEY$1 = '.data-api';
@@ -6695,7 +6695,8 @@
       if (this._element.parentNode &&
           this._element.parentNode.nodeType === Node.ELEMENT_NODE &&
           $__default["default"](this._element).hasClass(CLASS_NAME_ACTIVE) ||
-          $__default["default"](this._element).hasClass(CLASS_NAME_DISABLED)) {
+          $__default["default"](this._element).hasClass(CLASS_NAME_DISABLED) ||
+          this._element.hasAttribute('disabled')) {
         return
       }
 
@@ -6884,7 +6885,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v4.6.1): toast.js
+   * Bootstrap (v4.6.2): toast.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
@@ -6894,7 +6895,7 @@
    */
 
   const NAME$1 = 'toast';
-  const VERSION = '4.6.1';
+  const VERSION = '4.6.2';
   const DATA_KEY$1 = 'bs.toast';
   const EVENT_KEY$1 = `.${DATA_KEY$1}`;
   const JQUERY_NO_CONFLICT$1 = $__default["default"].fn[NAME$1];
