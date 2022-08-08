8bcb5f362a99f27968b7f0422581c94d972fc85f
Joe Parsons <parsonsj@arizona.edu>
2022-08-08 12:00:03 -0700

    ESLint fixes.

--- js/index.umd.js
+++ js/index.umd.js
@@ -5,33 +5,33 @@
  * --------------------------------------------------------------------------
  */
 
- import Alert from '../node_modules/bootstrap/js/src/alert.js'
- import Button from '../node_modules/bootstrap/js/src/button.js'
- import Carousel from '../node_modules/bootstrap/js/src/carousel.js'
- import Collapse from '../node_modules/bootstrap/js/src/collapse.js'
- import Dropdown from '../node_modules/bootstrap/js/src/dropdown.js'
- import Modal from '../node_modules/bootstrap/js/src/modal.js'
- import Offcanvas from '../node_modules/bootstrap/js/src/offcanvas.js'
- import Popover from '../node_modules/bootstrap/js/src/popover.js'
- import ScrollSpy from '../node_modules/bootstrap/js/src/scrollspy.js'
- import Tab from '../node_modules/bootstrap/js/src/tab.js'
- import Toast from '../node_modules/bootstrap/js/src/toast.js'
- import Tooltip from '../node_modules/bootstrap/js/src/tooltip.js'
-//  import Offcanvasmenu from './src/offcanvasmenu'
+import Alert from '../node_modules/bootstrap/js/src/alert.js'
+import Button from '../node_modules/bootstrap/js/src/button.js'
+import Carousel from '../node_modules/bootstrap/js/src/carousel.js'
+import Collapse from '../node_modules/bootstrap/js/src/collapse.js'
+import Dropdown from '../node_modules/bootstrap/js/src/dropdown.js'
+import Modal from '../node_modules/bootstrap/js/src/modal.js'
+import Offcanvas from '../node_modules/bootstrap/js/src/offcanvas.js'
+import Popover from '../node_modules/bootstrap/js/src/popover.js'
+import ScrollSpy from '../node_modules/bootstrap/js/src/scrollspy.js'
+import Tab from '../node_modules/bootstrap/js/src/tab.js'
+import Toast from '../node_modules/bootstrap/js/src/toast.js'
+import Tooltip from '../node_modules/bootstrap/js/src/tooltip.js'
+// import Offcanvasmenu from './src/offcanvasmenu'
 
 
- export default {
-   Alert,
-   Button,
-   Carousel,
-   Collapse,
-   Dropdown,
-   Modal,
-   Offcanvas,
-   Popover,
-   ScrollSpy,
-   Tab,
-   Toast,
-   Tooltip
-  //  Offcanvasmenu
- }
+export default {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Offcanvas,
+  Popover,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip
+ //  Offcanvasmenu
+}
