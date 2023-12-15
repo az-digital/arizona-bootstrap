c6b636b27005a6d64a43797f7570639ae1624c8e
mmunro-ltrr <mmunro@ltrr.arizona.edu>
2023-12-15 11:07:52 -0700

    Closes #1073: Update old brand links to the new marcom site. (#1075)
    Author: Zach Meyer <34076957+zsmeyer@users.noreply.github.com>

--- dist/js/arizona-bootstrap.bundle.js
+++ dist/js/arizona-bootstrap.bundle.js
@@ -7107,6 +7107,20 @@
     return Toast._jQueryInterface
   };
 
+  function _toPrimitive(t, r) {
+    if ("object" != typeof t || !t) return t;
+    var e = t[Symbol.toPrimitive];
+    if (void 0 !== e) {
+      var i = e.call(t, r || "default");
+      if ("object" != typeof i) return i;
+      throw new TypeError("@@toPrimitive must return a primitive value.");
+    }
+    return ("string" === r ? String : Number)(t);
+  }
+  function _toPropertyKey(t) {
+    var i = _toPrimitive(t, "string");
+    return "symbol" == typeof i ? i : String(i);
+  }
   function _defineProperties(target, props) {
     for (var i = 0; i < props.length; i++) {
       var descriptor = props[i];
@@ -7137,20 +7151,6 @@
       return target;
     };
     return _extends.apply(this, arguments);
-  }
-  function _toPrimitive(input, hint) {
-    if (typeof input !== "object" || input === null) return input;
-    var prim = input[Symbol.toPrimitive];
-    if (prim !== undefined) {
-      var res = prim.call(input, hint || "default");
-      if (typeof res !== "object") return res;
-      throw new TypeError("@@toPrimitive must return a primitive value.");
-    }
-    return (hint === "string" ? String : Number)(input);
-  }
-  function _toPropertyKey(arg) {
-    var key = _toPrimitive(arg, "string");
-    return typeof key === "symbol" ? key : String(key);
   }
 
   /**
