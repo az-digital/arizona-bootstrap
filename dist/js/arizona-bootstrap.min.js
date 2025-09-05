ee53b4cfad5b203788aaeb5beca396d0db9669e7
djcelaya <djcelaya@users.noreply.github.com>
2025-09-05 20:02:47 +0000

    5.0.0-beta1

--- dist/js/arizona-bootstrap.min.js
+++ dist/js/arizona-bootstrap.min.js
@@ -1,5 +1,5 @@
 /*!
-  * Arizona Bootstrap v5.0.0-alpha5 (https://github.com/az-digital/arizona-bootstrap)
+  * Arizona Bootstrap v5.0.0-beta1 (https://github.com/az-digital/arizona-bootstrap)
   * Copyright 2025 The Arizona Board of Regents on behalf of The University of Arizona
   * Licensed under MIT (https://github.com/az-digital/arizona-bootstrap/blob/main/LICENSE)
   */
