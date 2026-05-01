dc418316fa2dec349174d3367841cfa1e72ef46b
Chris Green <chrisgreen@arizona.edu>
2026-05-01 12:05:22 -0700

    Use CSS variables wherever possible instead of hard-coded values
    - Convert all color references in scss/custom/ and scss/override/ to CSS custom properties
    - Convert focus-ring Sass vars to CSS vars in _nav.scss, _carousel.scss, _dropdown.scss, _photo-gallery.scss
    - Convert border-radius Sass vars to CSS vars in _nav.scss, _button-group.scss
    - Convert $breadcrumb-item-padding-x to var(--bs-breadcrumb-item-padding-x)
    - Move btn sizing/spacing/radius overrides from Sass vars to .btn CSS var block in _buttons.scss
    - Convert btn box-shadow and focus-box-shadow to use CSS vars (--bs-white-rgb, --bs-black-rgb, --bs-focus-ring-width)
    - Convert text-bg-* classes to use --bs-text-bg-color and --bs-text-bg-bg tokens with shared base rule
    - Convert docs site SCSS (site/assets/scss/) brand color vars to CSS custom properties
    - Add --bd-purple-light CSS var to docs :root block
    
    Closes #1983

--- dist/js/arizona-bootstrap.bundle.js
+++ dist/js/arizona-bootstrap.bundle.js
@@ -7619,12 +7619,51 @@
     }
   }
 
-  /**
-   * --------------------------------------------------------------------------
-   * Arizona Bootstrap: navbar.js
-   * Licensed under MIT (https://github.com/az-digital/arizona-bootstrap/blob/main/LICENSE)
-   * --------------------------------------------------------------------------
-   */
+  function _arrayLikeToArray(r, a) {
+    (null == a || a > r.length) && (a = r.length);
+    for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e];
+    return n;
+  }
+  function _arrayWithHoles(r) {
+    if (Array.isArray(r)) return r;
+  }
+  function _iterableToArrayLimit(r, l) {
+    var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"];
+    if (null != t) {
+      var e,
+        n,
+        i,
+        u,
+        a = [],
+        f = true,
+        o = false;
+      try {
+        if (i = (t = t.call(r)).next, 0 === l) ; else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0);
+      } catch (r) {
+        o = true, n = r;
+      } finally {
+        try {
+          if (!f && null != t.return && (u = t.return(), Object(u) !== u)) return;
+        } finally {
+          if (o) throw n;
+        }
+      }
+      return a;
+    }
+  }
+  function _nonIterableRest() {
+    throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+  }
+  function _slicedToArray(r, e) {
+    return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest();
+  }
+  function _unsupportedIterableToArray(r, a) {
+    if (r) {
+      if ("string" == typeof r) return _arrayLikeToArray(r, a);
+      var t = {}.toString.call(r).slice(8, -1);
+      return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0;
+    }
+  }
 
   var HOVER_MEDIA_QUERY = '(hover: hover) and (pointer: fine)';
   var HIDE_DELAY_MS = 300;
@@ -7875,9 +7914,8 @@
 
     // Hide on hover-out, but never when the menu is click-open.
     _scheduleHide() {
-      var {
-        source
-      } = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+      var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+        source = _ref.source;
       this._cancelScheduledHide();
       if (this._clickOpen) {
         return;
@@ -8000,7 +8038,10 @@
           }
         }
       }
-      for (var [collapse, wasShown] of this._initialCollapseStates) {
+      for (var _ref4 of this._initialCollapseStates) {
+        var _ref3 = _slicedToArray(_ref4, 2);
+        var collapse = _ref3[0];
+        var wasShown = _ref3[1];
         var isShown = collapse.classList.contains('show');
         if (wasShown === isShown) {
           continue;
